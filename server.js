b6b78f5a3cd96a92bb3ac9fff7eb1d8622245d56
athanrbl <148010540+athanrbl@users.noreply.github.com>
2024-01-24 14:46:17 +0200

    Finalized

--- server.js
+++ server.js
@@ -6,12 +6,23 @@ const port = 3000
 
 const jsonData = JSON.parse(fs.readFileSync('src/mapCredentials.json'));
 
-app.get('/', (req, res) => {
+app.get('/', (res) => {
   res.send("Home")
 })
 
 app.get('/mapCreds', cors(), (req, res) => {
   res.send(jsonData)
+})
+
+app.get('/StationSVGs/List', cors(), (req, res) => {
+  const stationList = fs.readdir(`/src/`);
+  res.send(stationList);
+})
+
+app.get('/StationSVGs/:station', cors(), (req, res) => {
+  const station = req.params.station;
+  const file = `/src/${station}.svg`;
+  res.sendFile(__dirname + file);
 })
 
 app.listen(port, () => {
